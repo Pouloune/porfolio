2a8469217414e4c019cbc40231fb5b4f9622048b
Pouloune <dasseshella@gmail.com>
2024-01-20 23:09:43 -0500

    Update of website portfolio

--- script.js
+++ script.js
@@ -26,3 +26,117 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+function socialmedia(id,size){
+  const soc=document.getElementById(id);
+  const img= document.createElement("img");
+  img.setAttribute("src",`html_finalprojimages/${id}.svg`);
+  img.setAttribute("alt","SVG Icon");
+  img.setAttribute("width",`${size}`);
+  img.setAttribute("heigth",`${size}`);
+  
+  soc.appendChild(img);
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  const tag = document.getElementById('tag');
+  const words = JSON.parse(tag.getAttribute('data-words'));
+  const wait = parseInt(tag.getAttribute('data-wait'), 10);
+
+  let wordIndex = 0;
+  let letterIndex = 0;
+
+  function type() {
+      if (letterIndex < words[wordIndex].length) {
+          const span = document.querySelector('.txt');
+          span.textContent += words[wordIndex].charAt(letterIndex);
+          letterIndex++;
+          setTimeout(type, 100); // Adjust the timeout for the typing speed
+      } else {
+          setTimeout(erase, wait);
+      }
+  }
+
+  function erase() {
+      if (letterIndex > 0) {
+          const span = document.querySelector('.txt');
+          span.textContent = words[wordIndex].substring(0, letterIndex - 1);
+          letterIndex--;
+          setTimeout(erase, 50); // Adjust the timeout for the erasing speed
+      } else {
+          wordIndex++;
+          if (wordIndex >= words.length) {
+              wordIndex = 0;
+          }
+          setTimeout(type, 500); // Adjust the timeout before starting to type the next word
+      }
+  }
+
+  setTimeout(type, 500); // Adjust the initial delay before starting to type
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  AOS.init();
+  appearElementNav();
+  socialmedia("linkedin","16px");
+  socialmedia("github","20px");
+  socialmedia("facebook","15px");
+  socialmedia("instagram","20px");
+  socialmedia("x","12px");
+  currentDate();
+  
+
+});
+
+window.addEventListener('click',function(event){
+  const navbarToggle = document.getElementById('navbarToggle');
+  const navbarLinks = document.getElementById('navbarLinks');
+  if(navbarToggle.style.display!=='none' && navbarLinks.style.display!=="none"){
+    const isClickInsideNavbar = navbarToggle.contains(event.target);    
+    if (!isClickInsideNavbar) {
+        navbarLinks.style.display = 'none';
+      
+    }
+  }
+});
+
+
+function appearElementNav(){
+  const navbarToggle = document.getElementById('navbarToggle');
+    const navbarLinks = document.getElementById('navbarLinks');
+
+    navbarToggle.addEventListener('click', function () {
+      navbarLinks.style.display='block';
+    
+    });
+}
+
+
+function downloadResume(){}
+
+function onMeetClick() {
+  window.open("https://calendly.com/dasseshella/30min", '_blank');
+}
+
+function currentDate() {
+  const current = new Date();
+  let monthName = current.toLocaleDateString('en-US', {
+      month: "long",
+  });
+
+  let date = current.getDate();
+
+  let dayName = current.toLocaleDateString('en-US', {
+      weekday: "long",
+  });
+
+  var monthText = document.getElementById("month");
+  monthText.innerHTML = monthName;
+
+  var dayText = document.getElementById("date");
+  dayText.innerHTML = date;
+
+  var weekDayText = document.getElementById("day");
+  weekDayText.innerText = dayName;
+}
+
+ 
